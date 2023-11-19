d7e9905aab349c59d3393648df3625a50e35b8ce
Naeem175547 <144101668+Naeem175547@users.noreply.github.com>
2023-11-19 22:05:12 +0530

    add js foundamental assignment

---
+++ java_script_fundamental_assignment/Introduction_to_js_operations/task_6.js
@@ -0,0 +1,9 @@
+// Input values
+let weight = 70;    // Replace with your weight in kilograms
+let height = 1.75;  // Replace with your height in meters
+
+// Calculate BMI
+let bmi = weight / (height * height);
+
+// Display the result
+console.log(bmi)
