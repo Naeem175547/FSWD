2246ba6bc4fb97be4cf6eae1222f6c23f70e3389
Naeem175547 <144101668+Naeem175547@users.noreply.github.com>
2023-11-19 21:44:25 +0530

    Add files via upload

---
+++ java_script_fundamental_assignment/Introduction_to_js_operations/task_5.js
@@ -0,0 +1,5 @@
+let principle=1000;
+let rate=5;
+let time=2;
+let simple_interest=principle*rate*time;
+console.log(simple_interest)
---
+++ java_script_fundamental_assignment/Introduction_to_js_operations/task_7.js
@@ -0,0 +1,7 @@
+
+let radius = 10; 
+
+let area = (22/7)*(radius*radius)
+
+
+console.log(area)
